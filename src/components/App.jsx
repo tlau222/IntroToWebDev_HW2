46d06da40e07316ee5fea00e557f5a9c284cf12d
Tiffany Lau <58575683+tlau222@users.noreply.github.com>
2021-01-05 17:10:54 -0600

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+    <h1>Successful initialization of your project! Delete this message and get started. May the force be with you!</h1>
+    </div>
+  );
+}
+
+export default App;
